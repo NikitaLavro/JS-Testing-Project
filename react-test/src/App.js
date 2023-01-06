318100edcf8b53bef117f31c5ad5b7f0be68143e
Nikita <lavro.nikita123@gmail.com>
2023-01-06 18:21:01 -0500

    example tests 2

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  });
+
   return (
     <div className="App">
+      {data && <div style={{ color: "red" }}>data</div>}
       <h1>Hello World</h1>
       <button>Click me</button>
       <input type="text" placeholder="input value..." />
--- react-test/src/App.test.js
+++ react-test/src/App.test.js
@@ -1,13 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("are element in the document", () => {
-  render(<App />);
-  const helloWorldElem = screen.getByText(/hello world/i);
-  const btn = screen.getByRole("button");
-  const input = screen.getByPlaceholderText(/input value/i);
+describe("TEST APP", () => {
+  test("are element in the document", () => {
+    render(<App />);
+    const helloWorldElem = screen.getByText(/hello world/i);
+    const btn = screen.getByRole("button");
+    const input = screen.getByPlaceholderText(/input value/i);
 
-  expect(helloWorldElem).toBeInTheDocument();
-  expect(btn).toBeInTheDocument();
-  expect(input).toBeInTheDocument();
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(btn).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+
+  test("example test2", async () => {
+    render(<App />);
+    //const helloWorldElem = screen.queryByText(/hello2/i);
+    //expect(helloWorldElem).toBeNull()
+    const dataElem = await screen.findByText(/data/i);
+    expect(dataElem).toBeInTheDocument();
+    expect(dataElem).toHaveStyle({ color: "red" });
+  });
 });
