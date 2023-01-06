30ce55eae8e94436bb197eedf4bb7ee64fb79fec
Nikita <lavro.nikita123@gmail.com>
2023-01-06 18:05:18 -0500

    are element in the document test

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello World</h1>
+      <button>Click me</button>
+      <input type="text" placeholder="input value..." />
     </div>
   );
 }
--- react-test/src/App.test.js
+++ react-test/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("are element in the document", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWorldElem = screen.getByText(/hello world/i);
+  const btn = screen.getByRole("button");
+  const input = screen.getByPlaceholderText(/input value/i);
+
+  expect(helloWorldElem).toBeInTheDocument();
+  expect(btn).toBeInTheDocument();
+  expect(input).toBeInTheDocument();
 });
