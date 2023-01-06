ad54c344c698c83625093a9126a06765a7550dbc
Nikita <lavro.nikita123@gmail.com>
2023-01-06 09:14:08 -0500

    spyOn and toBeCalled times implementation

--- src/squre/square.test.js
+++ src/squre/square.test.js
@@ -1,17 +1,21 @@
 const square = require("./square");
 
 describe("square", () => {
-  let mock;
-  test("Correct Value", () => {
-    expect(square(2)).toBe(4);
-  });
-  test("Less Than", () => {
-    expect(square(2)).toBeLessThan(5);
-  });
-  test("Greater then", () => {
-    expect(square(2)).toBeGreaterThan(3);
-  });
-  test("Not to be Undefined", () => {
-    expect(square(2)).not.toBeUndefined();
+  //   test("Correct Value", () => {
+  //     expect(square(2)).toBe(4);
+  //   });
+  //   test("Less Than", () => {
+  //     expect(square(2)).toBeLessThan(5);
+  //   });
+  //   test("Greater then", () => {
+  //     expect(square(2)).toBeGreaterThan(3);
+  //   });
+  //   test("Not to be Undefined", () => {
+  //     expect(square(2)).not.toBeUndefined();
+  //   });
+  test("times amount", () => {
+    const spyMathPow = jest.spyOn(Math, "pow");
+    square(2);
+    expect(spyMathPow).toBeCalledTimes(1);
   });
 });
