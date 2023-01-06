cba95aa049e3b79376aa80af79c3f5caa311569d
Nikita <lavro.nikita123@gmail.com>
2023-01-06 08:55:56 -0500

    mapArrToStrings testing scenarios

--- src/mapArrToString/mapArrToStrings.test.js
+++ src/mapArrToString/mapArrToStrings.test.js
@@ -4,4 +4,17 @@ describe("mapArrToStrings", () => {
   test("Correct value", () => {
     expect(mapArrToStrings([1, 2, 3])).toEqual(["1", "2", "3"]);
   });
+  test("Random values", () => {
+    expect(mapArrToStrings([1, 2, 3, null, undefined, "asfsdgfdsf"])).toEqual([
+      "1",
+      "2",
+      "3",
+    ]);
+  });
+  test("Empty array", () => {
+    expect(mapArrToStrings([])).toEqual([]);
+  });
+  test("Negation", () => {
+    expect(mapArrToStrings([1, 2, 3])).not.toEqual([1, 2, 3, 4]);
+  });
 });
